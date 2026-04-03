7fa967495a66ad0cfa11cafc60338f0a6fe6ab63
Apriansyah-Dimas <muhmmddimas13@gmail.com>
2026-04-03 21:43:28 +0700

    dsd

--- components/home/LogoutButton.tsx
+++ components/home/LogoutButton.tsx
@@ -19,7 +19,7 @@ export function LogoutButton() {
         className="relative inline-block align-middle text-black font-bold text-sm px-6 py-3 cursor-pointer border-[3px] border-black rounded-[30px] uppercase tracking-widest transition-colors duration-700 bg-transparent z-2 overflow-hidden group hover:text-white"
       >
         <span className="relative z-10">LOGOUT</span>
-        <div className="c-button__blobs absolute -top-1 -left-1 w-[calc(100%+8px)] h-[calc(100%+8px)] pointer-events-none z-[-1] rounded-[30px]">
+        <div className="c-button__blobs absolute -top-1 -left-1 w-[calc(100%+8px)] h-[calc(100%+8px)] pointer-events-none z-[-1] rounded-[30px]" style={{ filter: 'url(#goo2)' }}>
           <div className="absolute w-[40%] h-[140%] bg-black rounded-full top-0 left-0 transition-transform duration-700 translate-y-[140%] group-hover:translate-y-[-10px]" />
           <div className="absolute w-[40%] h-[140%] bg-black rounded-full top-0 left-[32%] transition-transform duration-700 delay-[60ms] translate-y-[140%] group-hover:translate-y-[-10px]" />
           <div className="absolute w-[40%] h-[140%] bg-black rounded-full top-0 left-[65%] transition-transform duration-700 delay-[25ms] translate-y-[140%] group-hover:translate-y-[-10px]" />
